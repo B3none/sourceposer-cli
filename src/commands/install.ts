47e9054b58760ace39b82c10ab71e3846493f8a3
B3none <ablackham2000@gmail.com>
2022-06-23 19:02:38 +0100

    continued implementing todos

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -3,21 +3,27 @@ import {Command} from '@oclif/core'
 export class Install extends Command {
   static description = 'install a new plugin'
 
+  static args = [{name: 'plugin', description: 'Plugin to install, example: b3none/retakes-autoplant'}]
+
   async run(): Promise<void> {
     // TODO: Implement using the steps below
+
+    // if plugin passed:
     // 1. check to make sure we're in a SourceMod directory with an existing sourceposer.json
     // 2. if we are in a SourceMod directory with no sourceposer.json ask the user if they would like to initialise one
     // 3. check the existing sourceposer.json for the plugin we want to install
 
-    // 4. if already installed check the current version against the latest github release version
-    // 5. if not on the latest version update the plugin to the latest version
-    // 6. if update needed, perform update, then tell the user whether or not the plugin was installed successfully
-    // 7. if no update needed, then tell the user they are on the latest version already
+    // 4. if already installed then suggest that the user update it instead
 
-    // 8. if no version installed run installation and don't do any checks for existing plugin to prevent overwrites
+    // 5. if no version installed run installation and don't do any checks for existing plugin to prevent overwrites
     //    then tell the user that a new version was installed
 
-    // 9. update the sourceposer.json file with the installed version
+    // 6. update the sourceposer.json file with the installed version
+
+    // if no plugin passed:
+    // 1. compare current sourceposer.json against sourceposer.lock
+    // 2. if no changes detected do nothing
+    // 3. if changes detected install all plugins that have changed
 
     this.log('implement install command')
   }
--- src/commands/update.ts
+++ src/commands/update.ts
@@ -4,6 +4,20 @@ export class Update extends Command {
   static description = 'update existing plugins'
 
   async run(): Promise<void> {
+    // TODO: Implement using the steps below
+    // 1. check to make sure we're in a SourceMod directory with an existing sourceposer.json
+    // 2. if we are in a SourceMod directory with no sourceposer.json  suggest that the user initialise one first
+    // 3. check the existing sourceposer.json for the plugin we want to update
+
+    // 4. if not already installed then suggest that the user install it instead
+
+    // 5. check the current version against the latest github release version
+    // 6. if not on the latest version update the plugin to the latest version
+    // 7. if update needed, perform update, then tell the user whether or not the plugin was installed successfully
+    // 8. if no update needed, then tell the user they are on the latest version already
+
+    // 9. update the sourceposer.json file with the installed version
+
     this.log('implement update command')
   }
 }
