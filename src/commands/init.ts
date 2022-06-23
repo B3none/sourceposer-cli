f53157c4ef912b524b4be8bf558420016f296c99
B3none <ablackham2000@gmail.com>
2022-06-23 18:49:42 +0100

    added todos for init.ts

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -4,6 +4,16 @@ export class Init extends Command {
   static description = 'initialise sourceposer for this SourceMod installation'
 
   async run(): Promise<void> {
+    // TODO: Implement using the steps below
+    // 1. check to make sure we're in a SourceMod directory
+    // 2. notify the user if there's already a sourceposer.json or one close by
+    // 3. ask the user for the name (default sourceposer)
+    // 4. ask the user for the version (default 0.0.0)
+    // 5. ask the user for the description (default a sourceposer managed server)
+
+    // Nice to have
+    // 6. ask the user if there's any plugins they would like to install to begin
+
     this.log('implement init command')
   }
 }
