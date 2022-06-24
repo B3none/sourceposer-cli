ad0b5578f93c23d236b8d3ad26caee32f17d66f3
B3none <ablackham2000@gmail.com>
2022-06-25 00:55:15 +0100

    add config helper

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,6 +1,20 @@
+import {writeFile, access} from 'node:fs/promises'
 import {ConfigType} from '../types/config'
-import baseConfig from '../templates/sourceposer.base'
 
-export async function generateBaseConfig(): Promise<ConfigType|void> {
-  console.log(JSON.stringify(baseConfig))
+export const baseConfig = {
+  name: 'sourceposer',
+  version: '1.0.0',
+  description: 'a sourceposer managed server',
+  author: 'John Doe <john.doe@johndoe.com>',
+  plugins: {},
+} as ConfigType
+
+export async function hasConfigFile(): Promise<boolean> {
+  return access(process.cwd() + '/sourceposer.json')
+  .then(() => true)
+  .catch(() => false)
+}
+
+export async function generateBaseConfig(config: ConfigType): Promise<void> {
+  await writeFile(process.cwd() + '/sourceposer.json', JSON.stringify(config, null, 4))
 }
