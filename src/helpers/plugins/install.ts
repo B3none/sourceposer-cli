c2d3e9bffacb1241414758e3e6a66a7ec6033027
Wiggyy <fearscream69@gmail.com>
2022-06-27 18:29:58 +0100

    added temp directory creation logic

--- src/helpers/plugins/install.ts
+++ src/helpers/plugins/install.ts
@@ -1,4 +1,6 @@
 import { processTarball } from "./tarball"
+import * as fs from 'fs';
+import * as fsPromises from 'fs/promises';
 
 export async function installPlugins(plugins: Record<string, string>): Promise<(string|null)[]> {
   const promises: Promise<string|null>[] = []
@@ -12,6 +14,19 @@ export async function installPlugins(plugins: Record<string, string>): Promise<(
 
 // TODO: implement
 export async function installPlugin(plugin: string, version: string): Promise<string|null> {
-  await processTarball(plugin, version) 
+  const tempDirectory: string = process.cwd() + "/tmp/";
+  console.log("I'M HERE");
+  fsPromises.access(tempDirectory, fs.constants.F_OK)
+   .catch(async() => {
+    console.log("making directory", tempDirectory);
+    await fs.mkdir(tempDirectory, { recursive: true }, function(err) {
+        if (err) {
+          console.log(err)
+        }
+      })
+    });
+
+  await processTarball(tempDirectory, plugin, version,) 
+
   return ''
 }
--- src/helpers/plugins/tarball.ts
+++ src/helpers/plugins/tarball.ts
@@ -1,16 +1,27 @@
 import * as decompress from 'decompress'
+import * as https from 'https'
+import * as fs from 'fs'
 
 function getTarballUrl(plugin: string, version: string): string {
   return `https://api.github.com/repos/${plugin}/tarball/refs/tags/${version}`
 }
 
-async function downloadTarball(plugin: string, version: string) {
-  const tarballUrl = getTarballUrl(plugin, version)
-
-
-}
-
-export async function processTarball(plugin: string, version: string): Promise<boolean> {
+export async function processTarball(tempDirectory: string, plugin: string, version: string): Promise<boolean> {
   console.log("process tarball")
+  const tarballUrl = getTarballUrl(plugin, version)
+  console.log(tarballUrl)
+
+  https.get(tarballUrl, (res) => {
+    const path = tempDirectory + plugin + "-" + version + ".tar.gz";
+    const writeStream = fs.createWriteStream(path);
+  
+    res.pipe(writeStream);
+  
+    writeStream.on("finish", () => {
+      writeStream.close();
+      console.log("Download Completed");
+    });
+  })
+  
   return true
 }
