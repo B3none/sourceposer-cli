48ed4b9bad75cef3b08209c60a8d323be350914c
B3none <ablackham2000@gmail.com>
2022-06-28 01:01:54 +0100

    continued updates, we're getting closer to processing the tar.gz extraction

--- src/helpers/plugins/tarball.ts
+++ src/helpers/plugins/tarball.ts
@@ -1,11 +1,20 @@
-import * as fs from 'node:fs'
 import {createIfDoesNotExist} from '../directory'
+import * as fs from 'node:fs'
 import * as request from 'request'
-import * as zlib from 'node:zlib'
 import * as tar from 'tar-stream'
+import {ungzip} from 'node-gzip'
+import {Readable} from 'node:stream'
 
 function getTarballUrl(plugin: string, version: string): string {
   return `https://api.github.com/repos/${plugin}/tarball/refs/tags/${version}`
+}
+
+function getDirectory(plugin: string): string {
+  return `${process.cwd()}/tmp/${plugin}`
+}
+
+function getTarball(plugin: string, version: string): string {
+  return `${getDirectory(plugin)}/${version}.tar.gz`
 }
 
 export async function downloadTarball(plugin: string, version: string): Promise<void> {
@@ -13,8 +22,8 @@ export async function downloadTarball(plugin: string, version: string): Promise<
 
   const tarballUrl = getTarballUrl(plugin, version)
 
-  const directory = `${process.cwd()}/tmp/${plugin}/`
-  const localTarball = `${directory}${version}.tar.gz`
+  const directory = `${process.cwd()}/tmp/${plugin}`
+  const localTarball = `${directory}/${version}.tar.gz`
   await createIfDoesNotExist(directory)
 
   const response = await request({
@@ -31,16 +40,27 @@ export async function downloadTarball(plugin: string, version: string): Promise<
 export async function processTarball(plugin: string, version: string): Promise<void> {
   console.log('process tarball')
   // TODO: implement the following
-  const directory = `${process.cwd()}/tmp/${plugin}/`
-  const localTarball = `${directory}${version}.tar.gz`
-  // 1. decompress the downloaded tar https://github.com/kevva/decompress#api
-  try {
-    fs.createReadStream(localTarball)
-      .pipe(zlib.createGunzip())
-      .pipe(tar.extract())
-  } catch (e) {
-    console.error(e)
-  }
+  const localTarball = getTarball(plugin, version)
+
+  const compressedContents = fs.readFileSync(localTarball)
+
+  const decompressed = await ungzip(compressedContents)
+
+  console.log(decompressed.toJSON())
+
+  const extract = tar.extract()
+  .on('entry', (header, stream, callback) => {
+    // make directories or files depending on the header here...
+    // call callback() when you're done with this entry
+
+    console.log('file', header, stream, callback)
+  })
+  .on('finish', () => {
+    console.log('done!')
+  })
+
+  const stream = Readable.from(decompressed.toString())
+  stream.pipe(extract)
 
   // 2. scan the directories within the decompressed files - i've already written a function to do this in helpers/directory.ts getSubDirectories()
 
